841289242200cb1cc189875553d3cd9defab172b
bestupdateor <ballot669@gmail.com>
2026-09-17 05:18:14 -0400

    Update config.js
    window.SUPABASE_URL='https://emtbpgccdhuvxhoswqul.supabase.co';
    window.SUPABASE_KEY='YOUR_PUBLISHABLE_KEY_HERE';

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 window.SUPABASE_URL='https://emtbpgccdhuvxhoswqul.supabase.co';
-window.SUPABASE_KEY='sb_publishable_a5aT_zQZYs52WEm_-81__g_Nb6kcodt';
+window.SUPABASE_KEY='YOUR_PUBLISHABLE_KEY_HERE';
